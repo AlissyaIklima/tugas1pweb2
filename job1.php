2e799f1a3360528ee5664775334d72efa12fa16a
AlissyaIklima <alissyaiklim135600@gmail.com>
2024-08-27 10:06:04 +0700

    update

--- job1.php
+++ job1.php
@@ -8,16 +8,21 @@ class Mahasiswa{
     public $nama;
     public $nim;
     public $jurusan;
-    //construct 
+    //construct menjadi koeksi dimana didalamnya terdapat properti nama, nim, jurusan 
     public function __construct($nama, $nim, $jurusan){
         $this->nama = $nama;
         $this->nim = $nim;
         $this->jurusan = $jurusan;
     }
+    // metode tampilkanData yang bersifat public yang dimana bisa dilihat oleh orang lain
     public function tampilkanData(){
+        // memanggil kembali nama, nim, jurusan
         return "nama : $this->nama, nim : $this->nim, jurusan : $this->jurusan";
     }
 }
+// objek
+// membuat objek baru dan menimpan di properti $mahasiswa1
 $mahasiswa1 = new Mahasiswa('candra', '121314', 'Teknik Informatika');
+// menampilkan properti $mahasiswa1 dengan memanggil tampilkanData()
 echo $mahasiswa1->tampilkanData();
 ?>
